778aca5f7355f5692dbd8afc45e3879fc31305ee
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-09-08 09:24:18 -0700

    removed comments

--- src/components/Disappear.js
+++ src/components/Disappear.js
@@ -10,22 +10,10 @@ const FadeWords = styled.div`
   height: 100%;
 `;
 
-/*const timeRe = async (set, time) => {
-  for (let i = 0; i < 2; i++)
-    new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        set(time + 1);
-        console.log(time);
-        resolve('foo');
-      }, 1000);
-    });
-};*/
-
 const Disappear = props => {
   const [fade, setFade] = useState(0);
   const [start, setStart] = useState(0);
   const fadeClass = classNames({ fade: start && fade, nofade: !start });
-  //timeRe(setTime, time);
   return (
     <FadeWords className={fadeClass}>
       <Words reset={setFade} fade={fade} start={setStart} />
